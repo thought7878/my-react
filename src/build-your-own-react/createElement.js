b6b634b1786da12b2bc0f93878c96808e9d69dd2
Calm Andy <thought7878@qq.com>
2025-05-14 07:09:53 +0800

    feat: Concurrent Mode

--- src/build-your-own-react/createElement.js
+++ src/build-your-own-react/createElement.js
@@ -43,8 +43,35 @@ function render(element, container) {
       dom[name] = element.props[name];
     });
 
+  /* 
   // 递归地对每个子元素做同样的处理
   element.props.children.forEach((child) => render(child, dom));
+  */
+  let nextUnitOfWork = null;
+  function workLoop(deadline) {
+    // 标记是否应该停止工作
+    let shouldYield = false;
+    while (nextUnitOfWork && !shouldYield) {
+      nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+      // 检查是否还有剩余时间，如果没有，设置 shouldYield 为 true，以便在下一次循环中继续工作。
+      // 这是一个简单的时间切片实现，它允许浏览器在处理完一部分工作后，有机会处理其他任务，如响应用户输入或更新屏幕。
+
+      // requestIdleCallback 还给我们提供了一个截止日期参数。
+      // 我们可以用它来检查浏览器再次需要控制之前还有多少时间。
+      shouldYield = deadline.timeRemaining() < 1;
+    }
+    // 使用 requestIdleCallback 来创建一个循环。
+    // 你可以把 requestIdleCallback 看作是一个 setTimeout ，但不是由我们告诉它何时运行，
+    // 而是当主线程空闲时浏览器会运行回调。
+    requestIdleCallback(workLoop);
+  }
+  // React 不再使用 requestIdleCallback 了。现在它使用调度器包。但对于这个用例，在概念上是一样的。
+  requestIdleCallback(workLoop);
+  // 执行工作单元
+  // 该函数不仅执行工作，还返回下一个工作单元
+  function performUnitOfWork(nextUnitOfWork) {
+    // TODO
+  }
 
   // 将新节点追加到容器中
   container.appendChild(dom);
